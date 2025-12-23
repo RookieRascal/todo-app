333d7bdd40b3a0a91332337dcdc7620de5c14aa4
RookieRascal <autono999@gmail.com>
2025-12-23 16:45:04 +0000

    add

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
 const API = "https://todo-app-13bh.onrender.com";
+
 const todoRoutes = require("./routes/todoRoutes");
 const express = require("express");
 
@@ -39,4 +40,45 @@ async function addTodo() {
 					}
 
 					loadTodos();
+
+
+async function loadTodos() {
+	  const res = await fetch(API);
+	  const todos = await res.json();
+
+	  const list = document.getElementById("list");
+	  list.innerHTML = "";
+
+	  todos.forEach(todo => {
+		      const li = document.createElement("li");
+		      li.innerHTML = `
+		            ${todo.title}
+			          <button onclick="deleteTodo('${todo._id}')">Delete</button>
+				      `;
+		      list.appendChild(li);
+		    });
+}
+
+async function addTodo() {
+	  const task = document.getElementById("task").value;
+	  if (!task) return;
+
+	  await fetch(API, {
+		      method: "POST",
+		      headers: { "Content-Type": "application/json" },
+		      body: JSON.stringify({ title: task })
+		    });
+
+	  document.getElementById("task").value = "";
+	  loadTodos();
+}
+
+async function deleteTodo(id) {
+	  await fetch(`${API}/${id}`, {
+		      method: "DELETE"
+		    });
+	  loadTodos();
+}
+
+loadTodos();
 
